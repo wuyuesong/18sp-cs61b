53b996f81df637adbd8ae6afc4a4d023c6304723
wys <1377030423@qq.com>
2022-08-19 12:52:32 +0800

    fixed lab2

--- lab2/Intlist/IntList.java
+++ lab2/Intlist/IntList.java
@@ -82,6 +82,8 @@ public class IntList {
 
     public static IntList dcatenate(IntList A, IntList B) {
         //TODO:  fill in method
+        if(A == null) return B;
+        if(B == null) return A;
         IntList it = A;
         while (it.rest != null) {
             it = it.rest;
